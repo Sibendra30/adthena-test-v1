876d7ec077125ca2e079c46035fb851868d71461
Sibendra Singh <sibendra30singh@gmail.com>
2021-06-14 09:46:37 +0100

    Adding test for confirmation boxes

--- src/components/generic-components/__test__/Confirmation.test.jsx
+++ src/components/generic-components/__test__/Confirmation.test.jsx
@@ -3,19 +3,21 @@ import { render } from '@testing-library/react';
 import Confirmation from '@components/generic-components/Confirmation';
 
 describe('Testing Confirmation Component', () => {
-  it('Confirmation component should render', () => {
+  let containerComp;
+  beforeEach(() => {
     const { container } = render(<Confirmation />);
-    expect(container).toMatchSnapshot();
+    containerComp = container;
+  });
+  it('Confirmation component should render', () => {
+    expect(containerComp).toMatchSnapshot();
   });
 
   it('Should load two buttons', () => {
-    const { container } = render(<Confirmation />);
-    expect(container.querySelectorAll('button').length).toEqual(2);
+    expect(containerComp.querySelectorAll('button').length).toEqual(2);
   });
 
   it.only('Should have confirmation header and body', () => {
-    const { container } = render(<Confirmation />);
-    expect(container.querySelectorAll('.confirm-warning-header').length).toEqual(1);
-    expect(container.querySelectorAll('.confirmation-body').length).toEqual(1);
+    expect(containerComp.querySelectorAll('.confirm-warning-header').length).toEqual(1);
+    expect(containerComp.querySelectorAll('.confirmation-body').length).toEqual(1);
   });
 });
--- src/components/generic-components/__test__/DetailedConfirmation.test.jsx
+++ src/components/generic-components/__test__/DetailedConfirmation.test.jsx
@@ -3,22 +3,23 @@ import { render } from '@testing-library/react';
 import DetailedConfirmation from '@components/generic-components/DetailedConfirmation';
 
 describe('Testing Detailed Confirmation Component', () => {
-  it('Detailed Confirmation component should render', () => {
+  let containerComp;
+  beforeEach(() => {
     const { container } = render(<DetailedConfirmation />);
-    expect(container).toMatchSnapshot();
+    containerComp = container;
+  });
+  it('Detailed Confirmation component should render', () => {
+    expect(containerComp).toMatchSnapshot();
   });
   it('Should load two buttons', () => {
-    const { container } = render(<DetailedConfirmation />);
-    expect(container.querySelectorAll('button').length).toEqual(2);
+    expect(containerComp.querySelectorAll('button').length).toEqual(2);
   });
   it('Should load delete text box', () => {
-    const { container } = render(<DetailedConfirmation />);
-    expect(container.querySelectorAll('input[type="text"]').length).toEqual(1);
+    expect(containerComp.querySelectorAll('input[type="text"]').length).toEqual(1);
   });
   it('Should load confirmation header, body & history', () => {
-    const { container } = render(<DetailedConfirmation />);
-    expect(container.querySelectorAll('.confirm-warning-header').length).toEqual(1);
-    expect(container.querySelectorAll('.confirmation-details').length).toEqual(1);
-    expect(container.querySelectorAll('.confirmation-instr').length).toEqual(1);
+    expect(containerComp.querySelectorAll('.confirm-warning-header').length).toEqual(1);
+    expect(containerComp.querySelectorAll('.confirmation-details').length).toEqual(1);
+    expect(containerComp.querySelectorAll('.confirmation-instr').length).toEqual(1);
   });
 });
